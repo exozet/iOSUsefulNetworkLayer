215d14282aa85964e065c69abb00989938a64624
burak.uzunboy <b_uzunboy@outlook.com>
2020-02-10 11:35:16 +0100

    Renamed to UsefulNetworkLayer

--- Package.swift
+++ Package.swift
@@ -4,24 +4,24 @@
 import PackageDescription
 
 let package = Package(
-    name: "iOSUsefulNetworkLayer",
+    name: "UsefulNetworkLayer",
     platforms: [.iOS(.v10), .tvOS(.v10)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "iOSUsefulNetworkLayer",
-            targets: ["iOSUsefulNetworkLayer"]),
+            name: "UsefulNetworkLayer",
+            targets: ["UsefulNetworkLayer"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-         .package(url: "https://github.com/exozet/iOSCoreUsefulSDK", from: "1.0.5"),
+         .package(url: "https://github.com/exozet/iOSCoreUsefulSDK", from: "1.1.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "iOSUsefulNetworkLayer",
-            dependencies: ["iOSCoreUsefulSDK"]),
+            name: "UsefulNetworkLayer",
+            dependencies: ["CoreUsefulSDK"]),
 //        .testTarget(
 //        name: "iOSUsefulNetworkLayerTests",
 //        dependencies: ["iOSUsefulNetworkLayer", "iOSCoreUsefulSDK"]),
--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# iOSUsefulNetworkLayer
+# UsefulNetworkLayer for iOS
 
 ### A most useful Network Layer written for iOS Projects.
 
 ## Installation
 
-**iOSUsefulNetworkLayer** uses new Swift Package Manager which is easiest way introduced for iOS projects since from the beginning. 
+**UsefulNetworkLayer** uses new Swift Package Manager which is easiest way introduced for iOS projects since from the beginning. 
 
 From Xcode simply select `File > Swift Packages > Add Package Dependency...` and paste `https://github.com/exozet/iOSUsefulNetworkLayer` to search field. You can specify rules according to your preferences and you are ready to use. 
 
