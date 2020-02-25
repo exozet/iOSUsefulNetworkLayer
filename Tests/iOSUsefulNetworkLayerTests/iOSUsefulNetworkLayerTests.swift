629e7926e335fc1f034b59ef439e3bfb6fde61f4
burak.uzunboy <b_uzunboy@outlook.com>
2020-02-25 15:25:23 +0100

    Added test cases

--- Tests/iOSUsefulNetworkLayerTests/iOSUsefulNetworkLayerTests.swift
+++ Tests/iOSUsefulNetworkLayerTests/iOSUsefulNetworkLayerTests.swift
@@ -32,6 +32,38 @@ final class iOSUsefulNetworkLayerTests: XCTestCase {
                 XCTFail("Error: \(err.error.localizedDescription)")
                 break
             case .success(let obj):
+                print(obj)
+                exp.fulfill()
+            }
+        }
+        
+        wait(for: [exp], timeout: 5)
+    }
+    
+    func testNetworkLayerArray() {
+        let exp = XCTestExpectation(description: "Network layer should response success")
+
+        let api = APIConfiguration(hostURL: "https://jsonplaceholder.typicode.com",
+                                   endPoint: "todos",
+                                   requestType: .get,
+                                   headers: nil, body: nil,
+                                   responseBodyObject: [ExampleResponseObject].self,
+                                   priority: .low,
+                                   cachingTime: .init(seconds: 60),
+                                   isMainOperation: false, autoCache: true)
+        
+        guard let apiReq = api else {
+            XCTFail()
+            return
+        }
+        
+        apiReq.request { (result) in
+            switch result {
+            case .error(let err):
+                XCTFail("Error: \(err.error.localizedDescription)")
+                break
+            case .success(let obj):
+                print(obj)
                 exp.fulfill()
             }
         }
@@ -41,7 +73,8 @@ final class iOSUsefulNetworkLayerTests: XCTestCase {
 
     static var allTests = [
         ("testExample", testExample),
-        ("testNetworkLayer", testNetworkLayer)
+        ("testNetworkLayer", testNetworkLayer),
+        ("testNetworkLayerArray", testNetworkLayerArray)
     ]
 }
 
