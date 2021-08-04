245a7055fb02fd0fd4da95cb5de10a53e745a340
buzunboy <uzunboybu@gmail.com>
2021-08-04 13:58:27 +0200

    Added new request types

--- Sources/UsefulNetworkLayer/NetworkLayer/NetworkLayer+APIOperation.swift
+++ Sources/UsefulNetworkLayer/NetworkLayer/NetworkLayer+APIOperation.swift
@@ -100,6 +100,28 @@ extension NetworkLayer {
         case put = "PUT"
         /// delete type request
         case delete = "DELETE"
+        
+        case patch = "PATCH"
+        
+        case copy = "COPY"
+        
+        case head = "HEAD"
+        
+        case options = "OPTIONS"
+        
+        case link = "LINK"
+        
+        case unlink = "UNLINK"
+        
+        case purge = "PURGE"
+        
+        case lock = "LOCK"
+        
+        case unlock = "UNLOCK"
+        
+        case propfind = "PROPFIND"
+        
+        case view = "VIEW"
     }
     
 }
